2d3b8b5de494459aca6be3dec44447a091eb23e1
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-25 00:02:49 +0530

    mahindra

---
+++ src/components/FirstSlider.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
+import './FirstSlider.css'; // Import the custom CSS file
+
+const FirstSlider = () => {
+    return (
+        <>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 custom-img"
+                    src="Images/sliderimg1.jpg"
+                    alt="First slide"
+                />
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 custom-img"
+                    src="Images/sliderimg3.png"
+                    alt="Second slide"
+                />
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    className="d-block w-100 custom-img"
+                    src="Images/sliderimg3change.jpg"
+                    alt="Third slide"
+                />
+                
+            </Carousel.Item>
+        </Carousel>
+        <Container>
+                <h1 className="text-center my-4" style={{fontWeight:"bold"}}>Find Your Dream Cars</h1>
+            </Container>
+        </>
+    );
+}
+
+export default FirstSlider;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FirstSlider from '../components/FirstSlider'
+import CarsMahindra from '../components/CarsMahindra'
+function Home() {
+  return (
+    <>
+   <FirstSlider/>
+<CarsMahindra/>    
+    </>
+  )
+}
+
+export default Home
