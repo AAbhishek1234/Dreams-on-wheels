9d94c379cfff11e23e2448f0ba9ee7582f6a1875
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-25 00:44:44 +0530

    mahindra

--- src/components/FirstSlider.jsx
+++ src/components/FirstSlider.jsx
@@ -1,3 +1,56 @@
+// import React from 'react';
+// import { Carousel, Container } from 'react-bootstrap';
+// import './FirstSlider.css'; // Import the custom CSS file
+
+// const FirstSlider = () => {
+//     return (
+//         <>
+//         <Carousel>
+//             <Carousel.Item>
+//                 <img
+//                     className="d-block w-100 custom-img"
+//                     src="Images/sliderimg1.jpg"
+//                     alt="First slide"
+//                 />
+//             </Carousel.Item>
+
+//             <Carousel.Item>
+//                 <img
+//                     className="d-block w-100 custom-img"
+//                     src="Images/sliderimg3.png"
+//                     alt="Second slide"
+//                 />
+//             </Carousel.Item>
+
+//             <Carousel.Item>
+//                 <img
+//                     className="d-block w-100 custom-img"
+//                     src="Images/sliderimg3change.jpg"
+//                     alt="Third slide"
+//                 />
+                
+//             </Carousel.Item>
+//         </Carousel>
+//         <Container>
+//                 <h1 className="text-center my-4" style={{fontWeight:"bold"}}>Find Your Dream Cars</h1>
+//             </Container>
+//         </>
+//     );
+// }
+
+// export default FirstSlider;
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
 import './FirstSlider.css'; // Import the custom CSS file
@@ -5,34 +58,33 @@ import './FirstSlider.css'; // Import the custom CSS file
 const FirstSlider = () => {
     return (
         <>
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100 custom-img"
-                    src="Images/sliderimg1.jpg"
-                    alt="First slide"
-                />
-            </Carousel.Item>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 custom-img"
+                        src="Images/sliderimg1.jpg"
+                        alt="First slide"
+                    />
+                </Carousel.Item>
 
-            <Carousel.Item>
-                <img
-                    className="d-block w-100 custom-img"
-                    src="Images/sliderimg3.png"
-                    alt="Second slide"
-                />
-            </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 custom-img"
+                        src="Images/sliderimg3.png"
+                        alt="Second slide"
+                    />
+                </Carousel.Item>
 
-            <Carousel.Item>
-                <img
-                    className="d-block w-100 custom-img"
-                    src="Images/sliderimg3change.jpg"
-                    alt="Third slide"
-                />
-                
-            </Carousel.Item>
-        </Carousel>
-        <Container>
-                <h1 className="text-center my-4" style={{fontWeight:"bold"}}>Find Your Dream Cars</h1>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 custom-img"
+                        src="Images/sliderimg3change.jpg"
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+            <Container>
+                <h1 className="text-center my-4" style={{ fontWeight: "bold" }}>Find Your Dream Cars</h1>
             </Container>
         </>
     );
