c2b35e1f34904ba8e19b6b117c5e73a8a257b224
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-25 11:08:36 +0530

    mahindra

--- src/components/FirstSlider.jsx
+++ src/components/FirstSlider.jsx
@@ -1,42 +1,48 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
-import './FirstSlider.css'; // Import the custom CSS file
+import { Carousel } from 'react-bootstrap';
 
-const FirstSlider = () => {
+const ImageSlider = () => {
     return (
-        <>
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img"
-                        src="Images/sliderimg1.jpg"
-                        alt="First slide"
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img"
-                        src="Images/sliderimg3.png"
-                        alt="Second slide"
-                    />
-                </Carousel.Item>
-
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img"
-                        src="Images/sliderimg3change.jpg"
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
-            <Container>
-                <h1 className="text-center my-4" style={{ fontWeight: "bold" }}>Find Your Dream Cars</h1>
-            </Container>
-        </>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="/Images/sliderimg1.jpg" // Path to the first image
+                    alt="First slide"
+                    style={{height:"34.4rem"}}
+                />
+                
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="/Images/sliderimg3change.jpg" // Path to the second image
+                    alt="Second slide"
+                    style={{height:"34.4rem"}}
+                />
+                
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="/Images/sliderimg3.png" // Path to the third image
+                    alt="Third slide"
+                    style={{height:"34.4rem"}}
+                />
+               
+            </Carousel.Item>
+            {/* <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="/Images/sliderimg1.jpg" // Path to the fourth image
+                    alt="Fourth slide"
+                />
+                <Carousel.Caption>
+                    <h3>Fourth Slide Caption</h3>
+                </Carousel.Caption>
+            </Carousel.Item> */}
+        </Carousel>
     );
 }
 
-export default FirstSlider;
-
-
+export default ImageSlider;
