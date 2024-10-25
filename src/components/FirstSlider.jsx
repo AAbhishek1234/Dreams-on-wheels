bab88541ebfe7baacbe7c1b305d197af6303c106
AAbhishek1234 <er.abhishekrawat73@gmail.com>
2024-10-25 21:26:12 +0530

    mahindra new launch

--- src/components/FirstSlider.jsx
+++ src/components/FirstSlider.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
-import { Carousel,Container } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
 import './FirstSlider.css'; // Import your custom CSS
 
 const ImageSlider = () => {
   return (
     <>
-    <Carousel>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 responsive-image"
-          src="Images/all.jpg"
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 responsive-image"
-          src="Images/grp1.webp"
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 responsive-image"
-          src="Images/bmw.jpg"
-          alt="Third slide"
-        />
-      </Carousel.Item>
-    </Carousel>
-    <Container>
-    <h1 className="text-center" style={{fontWeight:"bold"}}>Find Your Dream Cars</h1>
-    </Container>
+      <Carousel interval={1500}> {/* Set interval to 3000 ms (3 seconds) */}
+        <Carousel.Item>
+          <img
+            className="d-block w-100 responsive-image"
+            src="Images/all.jpg"
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 responsive-image"
+            src="Images/grp1.webp"
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 responsive-image"
+            src="Images/bmw.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+      <Container>
+        <h1 className="text-center" style={{ fontWeight: "bold" }}>
+          Find Your Dream Cars
+        </h1>
+      </Container>
     </>
   );
 };
